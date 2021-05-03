d107e1ae19c86a07521caf0b258fbd1017f787f2
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-05-03 22:09:18 +0630

    #add: Home css fixes

--- components/Home/FAQ/FAQ.js
+++ components/Home/FAQ/FAQ.js
@@ -1,13 +1,14 @@
 import classes from "./FAQ.module.scss";
 
 function FAQ({ items }) {
+    console.log(items);
     return (
         <ul className={classes.FAQ}>
             {items.map((item, i) => (
                 <li key={i}>
                     <details>
                         <summary>{item.question}</summary>
-                        <p>{items.answer}</p>
+                        <p>{item.answer}</p>
                     </details>
                 </li>
             ))}
