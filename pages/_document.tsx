7077a0d7d8e91abc0e5fb37bebb438e8c863a75c
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-03-02 06:57:50 +0630

    add: _document

---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import { ReactElement } from "react";
+class BookmarkDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component,
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default BookmarkDocument;
