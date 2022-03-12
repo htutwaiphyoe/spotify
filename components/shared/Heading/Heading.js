5ec9c601d8b2f197c6859549e0225fc612dccbe1
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-03-12 15:28:33 +0630

    update: repo clean up

--- components/shared/Heading/Heading.js
+++
@@ -1,5 +0,0 @@
-import classes from "./Heading.module.scss";
-
-const Heading = (props) => <h1 className={classes.Heading}>{props.text}</h1>;
-
-export default Heading;
--- components/shared/Paragraph/Paragraph.js
+++
@@ -1,5 +0,0 @@
-import classes from "./Paragraph.module.scss";
-
-const Paragraph = (props) => <p className={`${classes.Paragraph}`}>{props.children}</p>;
-
-export default Paragraph;
