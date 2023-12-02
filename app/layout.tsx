057f8fa4e94963eca53f9b93f13c2f5f2643cd6c
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2023-12-02 22:15:38 +0630

    feat: repo setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify",
+  description: "Listen your favorite music in one place.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
