e0fdcea7f157fb2eb81a9ec037568982f69be83a
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2023-12-03 08:26:14 +0630

    feat: sidebar style

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,9 +1,39 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import { PropsWithChildren, useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
 type SidebarProps = PropsWithChildren & {};
 
 function Sidebar({ children }: SidebarProps) {
-  return <div>{children}</div>;
+  const pathname = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+        icon: HiHome,
+      },
+      {
+        label: "Search",
+        active: pathname === "/search",
+        href: "/search",
+        icon: BiSearch,
+      },
+    ],
+    [pathname]
+  );
+
+  return (
+    <aside className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full p-2 w-300"></div>
+      {children}d
+    </aside>
+  );
 }
 
 export default Sidebar;
