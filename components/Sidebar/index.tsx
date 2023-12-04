38dc7c8d60c83a147ba1535d977d092c25081e6e
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2023-12-04 17:51:49 +0630

    feat: SidebarItem

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -4,7 +4,8 @@ import { PropsWithChildren, useMemo } from "react";
 import { usePathname } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-import Box from "./Box";
+import Box from "../Box";
+import SidebarItem from "./SidebarItem";
 
 type SidebarProps = PropsWithChildren & {};
 
@@ -17,24 +18,34 @@ function Sidebar({ children }: SidebarProps) {
         label: "Home",
         active: pathname !== "/search",
         href: "/",
-        icon: HiHome,
+        Icon: HiHome,
       },
       {
         label: "Search",
         active: pathname === "/search",
         href: "/search",
-        icon: BiSearch,
+        Icon: BiSearch,
       },
     ],
     [pathname]
   );
 
   return (
-    <aside className="flex h-full">
-      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full p-2 w-300">
-        <Box className="overflow-y-auto h-full">{children}</Box>
-      </div>
-    </aside>
+    <main className="flex h-full">
+      <aside className="hidden md:flex flex-col gap-y-2 bg-black h-full p-2 w-300">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((route) => (
+              <SidebarItem key={route.label} {...route} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">Library</Box>
+      </aside>
+      <section className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+      </section>
+    </main>
   );
 }
 
