da00e9f985a789488304b078086a21284aa4d792
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2023-12-07 22:36:55 +0630

    feat: SupabaseProvider.tsx

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { PropsWithChildren, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { Database } from "@/types_db";
+
+type SupabaseProviderProps = PropsWithChildren;
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
