03c1bcd34872c2b66f54855fe71ae949d76706f1
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-01 22:00:44 +0700

    feat: refactor

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -9,21 +9,7 @@ impl Deck {
   fn new() -> Self {
     let mut cards = vec![];
     let suits = ["Spades", "Diamonds", "Hearts", "Clubs"];
-    let values = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-    ];
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
     for suit in suits {
       for value in values {
