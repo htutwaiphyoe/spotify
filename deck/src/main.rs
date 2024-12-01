075a2c9e631d7e3ab0c77b6368eed3007157cad4
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-01 15:21:11 +0700

    feat: inherent implementation and methods

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -3,31 +3,38 @@ struct Deck {
   cards: Vec<String>,
 }
 
-fn main() {
-  let mut cards = vec![];
-  let suits = ["Spades", "Diamonds", "Hearts", "Clubs"];
-  let values = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-  ];
+impl Deck {
+  fn new() -> Self {
+    let mut cards = vec![];
+    let suits = ["Spades", "Diamonds", "Hearts", "Clubs"];
+    let values = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
 
-  for suit in suits {
-    for value in values {
-      cards.push(format!("{} of {}", value, suit));
+    for suit in suits {
+      for value in values {
+        cards.push(format!("{} of {}", value, suit));
+      }
     }
-  }
 
-  let deck = Deck { cards };
+    return Deck { cards };
+  }
+}
+
+fn main() {
+  let deck = Deck::new();
+
   println!("Deck: {:#?}", deck);
 }
